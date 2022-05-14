39b4235f6f857ca5389c0f15e27a25f40918318e
fainnerramirez <fainnerramirez@gmail.com>
2022-05-14 11:16:45 -0500

    request api data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./App.css";
 
-function App() {
+var config = {
+  method: "get",
+  url: "https://api.countrystatecity.in/v1/countries",
+  headers: {
+    "X-CSCAPI-KEY": "API_KEY",
+  },
+};
+
+const App = () => {
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello wordl!</h1>
     </div>
   );
-}
+};
 
 export default App;
